d68419d6c78ce19417aaa2d88dceed2eac5d9440
banoshko <richard.vajko@icloud.com>
2026-09-19 23:31:28 +0200

    Phonebook finished

--- basic/phonebook.py
+++ basic/phonebook.py
@@ -7,37 +7,74 @@ try:
 except FileNotFoundError:
     contacts = {}
 
+allowed_number_chars = "0123456789+ "
+
+def is_valid_number(number):
+    for char in number:
+        if char not in allowed_number_chars:
+            return False
+    return True
+def save():
+    try:
+        with open("saves/phonebook_memory.json", "w") as f:
+            json.dump(contacts, f)
+    except FileNotFoundError:
+        print("File not found.")
+
 while True:
     try:
-        user = int(input("--------------------\n1. Add Contact\n2. Search Contacts\n3. Delete Contact\n4. Exit\n--------------------\nProvide your choice: "))
+        user = int(input("--------------------\n1. Add Contact\n2. View Contacts\n3. Delete Contact\n4. Exit\n--------------------\nProvide your choice: "))
         if user == 1:
-            try:
-                name = input("--------------------\nEnter name: ")
-                number = (input("Enter number: "))
-                print("--------------------")
-                contacts[name] = number
-                with open("saves/phonebook_memory.json", "w") as f:
-                    json.dump(contacts, f)
-                print(f"{name} has been added to your contacts!")
-            except Exception:
-                print("--------------------\nInvalid input.")
+            name = input("--------------------\nEnter name: ")
+            name = name.strip().capitalize()
+            number = input("Enter number: ").strip()
+            if number == "":
+                print("--------------------\nYou didn't provide a number!")
+            elif name == "":
+                print("--------------------\nYou didn't provide a name!")
+            elif not is_valid_number(number):
+                print("--------------------\nYou used invalid characters!")
+            else:
+                try:
+                    print("--------------------")
+                    contacts[name] = number
+                    save()
+                    print(f"{name} has been added to your contacts!")
+                except Exception:
+                    print("--------------------\nInvalid input.")
+
         elif user == 2:
-            try:
-                print(f"--------------------\nYour contacts are:\n{contacts}")
-            except:
-                print("--------------------\nInvalid input.")
+            if contacts == {}:
+                print("--------------------\nNo contacts yet!")
+            else:
+                    
+                position = 1
+
+                try:
+                    print("--------------------\nYour contacts:")
+                    for name in sorted(contacts, key=str.lower):
+                        print(f"{position}. {name} : {contacts[name]}")
+                        position += 1
+                except:
+                    print("Invalid input.")
+
         elif user ==3:
-            try:
-                name = input("--------------------\nWhat contact would you like to delete? ")
-                contacts.pop(name)
-                print(f"{name} has been removed from your contacts!")
-                with open("saves/phonebook_memory.json", "w") as f:
-                    json.dump(contacts, f)
-            except Exception:
-                print("--------------------\nInvalid input.")
+            name = input("--------------------\nWhat contact would you like to delete? ")
+            name = name.strip().capitalize()
+            if name not in contacts:
+                print("--------------------\nWe don't have him around here")
+            else:
+                try:
+                    contacts.pop(name)
+                    print(f"{name} has been removed from your contacts!")
+                    save()
+                except Exception:
+                    print("--------------------\nSomething went wrong")
+
         elif user == 4:
             print("--------------------\nBye bye\n--------------------")
             break
+
         else:
             print("--------------------\nInvalid input.")
     except ValueError:
